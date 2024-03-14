b2dd7cf4bcc9f052cbfeb5ceb8756fdacec96837
Prince Kumar Chaurasia <princechaurasiaofficial24@gmail.com>
2024-03-14 23:51:52 +0530

    NextJs template

---
+++ my-app/src/app/page.js
@@ -0,0 +1,12 @@
+import { FaLinkedin } from "react-icons/fa";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col text-6xl gap-10 items-center justify-center">
+      Template by : Prince Kumar Chaurasia.
+      <a href="https://www.linkedin.com/in/princechaurasia/">
+        <FaLinkedin />
+      </a>
+    </main>
+  );
+}
